471c40139454165708096a0293e704edaeece4a2
genusglobalinc <34900625+genusglobalinc@users.noreply.github.com>
2024-07-13 14:23:21 -0400

    Update gdd_form.py
    added temp test mail server to run through code. DEV NOT PROD. 
    
    do not send this code to prod!!

--- gdd_form.py
+++ gdd_form.py
@@ -5,11 +5,13 @@ import traceback
 app = Flask(__name__)
 
 # Configure Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.your-email-provider.com'
-app.config['MAIL_PORT'] = 587
+# Configure Flask-Mail for Mailtrap
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = 'b512f8a7b89a8d'
+app.config['MAIL_PASSWORD'] = '867b996a31c142'
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your-email@example.com'
-app.config['MAIL_PASSWORD'] = 'your-email-password'
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = 'your-email@example.com'
 
 # Initialize Flask-Mail
